d1f937644f9e0a9900b8d876faabf1f833cba5e8
121philip <ykx20010330@gmail.com>
2024-10-21 23:24:00 +0200

    new files

--- MS3/mqtt_client_demo.py
+++ MS3/mqtt_client_demo.py
@@ -4,22 +4,31 @@ import paho.mqtt.client as mqtt
 # The callback for when the client receives a CONNACK response from the server
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
+
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-
-    client.subscribe("$SYS/#")
+    client.subscribe("CoreElectronics/test")
+    client.subscribe("CoreElectronics/topic")
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
 
+    if msg.payload == "Hello":
+        print("Received message #1. do something")
+    if msg.payload == "World!":
+        print("Received message #2. do something else")
 
+
+# Create an MQTT client and attach our routines to it.
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("mqtt.eclipseprojects.io", 1883, 60)
+# mqtt.eclipseprojects.io
+client.connect("test.mosquitto.org", 1883, 60)
+
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
